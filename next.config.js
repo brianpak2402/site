909cb820c360d9c50225f120d0b6cb05e196b493
Brian Pak <brianpak2402@gmail.com>
2022-12-23 15:01:33 -0500

    remove github actions

--- next.config.js
+++ next.config.js
@@ -3,18 +3,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  assetPrefix: isProd ? '/brianpak2402.github.io/' : '',
-  images: {
-    unoptimized: true,
-  },
-  exportPathMap: function() {
-    return {
-      "/locales/de-DE/summer-dress-f": {
-        page: "/locales/[locale]/[slug]",
-        query: { locale: "de-DE", slug: "summer-dress-f" }
-      }
-    };
-  },
   experimental: {
     appDir: true,
   },
