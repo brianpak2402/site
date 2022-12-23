9d0723ff202922340c1917c64d98386e66f89d63
Brian Pak <brianpak2402@gmail.com>
2022-12-23 14:16:48 -0500

    update package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  basePath: '/brianpak2402.github.io',
+  assetPrefix: isProd ? '/brianpak2402.github.io/' : '',
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     appDir: true,
   },
