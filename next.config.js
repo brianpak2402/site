403c0d14dc549511b2812a6c49209f35e3d81c75
Brian Pak <brianpak2402@gmail.com>
2022-12-23 13:56:56 -0500

    attempt deploy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  assetPrefix: isProd ? '/brianpak2402.github.io/' : '',
   images: {
     unoptimized: true,
   },
