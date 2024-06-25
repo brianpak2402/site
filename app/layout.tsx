aee3c108037e43a75307aee1355c18fcd985c169
Brian Pak <brianpak2402@gmail.com>
2024-06-25 13:03:22 -0400

    feat: add shader gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Spotlight from "@components/custom/Spotlight";
 import Header from "@containers/Header";
 import "./globals.css";
 
